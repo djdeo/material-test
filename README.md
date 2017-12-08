524385b87f45eebd5e609f7a981b543a75d72cde
djdeo <deoseoer@gmail.com>
2017-12-08 17:08:56 +0800

    Vue material test

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # material-test
 Test material app with vue
+
+## Vue-cli
+
+`vue init webpack material-test`
+
+Then 
+
+`npm install`
+
+Then install vue-material
+
+`npm install vue-material@beta --save`
+
+Then replace the following files
+
+- index.html
+- App.vue
+- main.js
+
+Then run the dev
+
+`npm run dev`
+
+
+when waring message of `git add .` shows, uses the
+
+`git config core.autocrlf true` command to turn the warning off
---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="phone-viewport">
+      <md-bottom-bar md-sync-route>
+        <md-bottom-bar-item to="/components/bottom-bar/home" md-label="Home" md-icon="home"></md-bottom-bar-item>
+        <md-bottom-bar-item to="/components/bottom-bar/posts" md-label="Pages" md-icon="pages"></md-bottom-bar-item>
+        <md-bottom-bar-item to="/components/bottom-bar/favorites" md-label="Favorites" md-icon="favorite"></md-bottom-bar-item>
+      </md-bottom-bar>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'BarRouter'
+  }
+</script>
+
+<style>
+ body {
+  max-width: 480px;
+  margin: 0 auto;
+ }
+  .phone-viewport {
+    text-align: center;
+    width: 100%;
+    height: 500px;
+    display: inline-flex;
+    align-items: flex-end;
+    overflow: hidden;
+    border: 1px solid rgba(#000, .26);
+    background: rgba(#000, .06);
+  }
+</style>
